ac706989619ec1a3428fae933e22f66af89f0f3e
Mathieu <mathieu.delva@hotmail.com>
2024-11-25 10:10:14 +0100

    [16.0][ADD] purchase_split_by_route

---
+++ purchase_split_by_route/models/purchase_order.py
@@ -0,0 +1,29 @@
+# Copyright 2024 Akretion France (http://www.akretion.com/)
+# @author: Mathieu Delva <mathieu.delva@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    default_route_id = fields.Many2one("stock.route", store=True)
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    @api.model
+    def _prepare_purchase_order_line_from_procurement(
+        self, product_id, product_qty, product_uom, company_id, values, po
+    ):
+        vals = super()._prepare_purchase_order_line_from_procurement(
+            product_id, product_qty, product_uom, company_id, values, po
+        )
+        origin = values["move_dest_ids"].origin
+        order_id = self.env["sale.order"].search([("name", "=", origin)])
+        if order_id:
+            po.default_route_id = order_id.route_id.id
+
+        return vals
